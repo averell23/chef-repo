16ee4040d79f2419d5845a37dc473047fbf5089b
Daniel Hahn <dha@betterplace.org>
2014-02-11 19:45:30 +0100

    WIP

--- site-cookbooks/hetzner_one/recipes/default.rb
+++ site-cookbooks/hetzner_one/recipes/default.rb
@@ -17,6 +17,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+package "openssl"
+package "libssl-dev"
+
 swap_file '/swapfile' do
   size      2048    # MBs
 end
